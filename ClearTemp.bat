bf22955236a9a81efcc3bacbcc6d47f7dbf1cc4d
aguilatroll 1081 <aguilatroll1081@gmail.com>
2023-10-13 17:22:33 +0200

    Limpieza archivos temp Script

---
+++ ClearTemp.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Limpiando archivos temporales...
+rem Elimina archivos temporales en el sistema
+del /q /f %temp%\*.*
+rem Elimina archivos temporales del sistema
+del /q /f C:\Windows\Temp\*.*
+rem Elimina archivos temporales de Internet Explorer
+del /q /f "%userprofile%\AppData\Local\Microsoft\Windows\INetCache\*.*"
+rem Vacía la Papelera de reciclaje
+rd /s /q "%userprofile%\AppData\Local\Microsoft\Windows\Explorer\*.db"
+echo Limpieza completada.
